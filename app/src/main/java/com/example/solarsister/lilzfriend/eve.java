d4e80b25dc6b6e0940f7c594b236f6823b3fed21
TUMUSIIME <TUMUSIIME>
2017-03-23 16:06:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/solarsister/lilzfriend/eve.java
@@ -0,0 +1,13 @@
+package com.example.solarsister.lilzfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class eve extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eve);
+    }
+}
---
+++ app/src/main/java/com/example/solarsister/lilzfriend/jalia.java
@@ -0,0 +1,13 @@
+package com.example.solarsister.lilzfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class jalia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jalia);
+    }
+}
